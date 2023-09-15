508a4628c0e93ea621530a6891210143d4e31c5d
Rain <2215338912@qq.com>
2023-09-15 14:59:56 +0800

    1

--- IsoMesh/Assets/Source/SDFs/Editor/MeshSDFGenerator.cs
+++ IsoMesh/Assets/Source/SDFs/Editor/MeshSDFGenerator.cs
@@ -419,6 +419,12 @@ namespace IsoMesh.Editor
                 MeshSampleComputeShader.SetInt(Properties.VertexCount_Int, m_vertices.Length);
 
                 RunBoundsPhase(mesh, out minBounds, out maxBounds);
+                
+                var max = Mathf.Max(maxBounds.x, maxBounds.y, maxBounds.z,minBounds.x,minBounds.y,minBounds.z);
+                max = Mathf.CeilToInt(max);
+                maxBounds = new Vector3(max, max, max);
+                minBounds = new Vector3(-max, -max, -max);
+                
                 RunSamplePhase(hasUVs, out samples, out packedUVs, minBounds, maxBounds);
             }
 
